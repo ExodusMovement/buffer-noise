88e8d4ad67ba2e44ef11bb5b4976bd6215a522db
RyanZim <opensrc@ryanzim.com>
2017-04-10 15:17:44 -0400

    Add test for when data.length > size

--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 const test = require('tape')
+const crypto = require('crypto')
 const createExpander = require('..')
 
 const size = Math.pow(2, 15) // 32K
@@ -9,6 +10,15 @@ test('expand / shrink', (t) => {
   const data = Buffer.from('Hello World!')
   const expanded = expand(data)
   t.is(expanded.length, size, 'expands to correct length')
+  t.assert(shrink(expanded).equals(data), 'shrink(expand(data)) === data')
+  t.end()
+})
+
+test('expand / shrink when data is larger than the size', (t) => {
+  t.plan(1)
+  const { expand, shrink } = createExpander(size)
+  const data = crypto.randomBytes(size + 5)
+  const expanded = expand(data)
   t.assert(shrink(expanded).equals(data), 'shrink(expand(data)) === data')
   t.end()
 })
