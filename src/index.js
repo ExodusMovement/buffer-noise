0993e0cdd24eee4886561b2c5cc69a6c08debce9
Jan W <jan.wendland@t-online.de>
2024-11-14 17:56:49 +0100

    feat: use platform-independent `randomBytes` (#6)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-// @flow
-const crypto = require('crypto')
+const { randomBytes } = require('@exodus/crypto/randomBytes')
 
-module.exports = function (size: number): Object {
+/**
+ * @param {number} size
+ * @returns {{expand(Buffer): Buffer, shrink(Buffer): Buffer}}
+ */
+module.exports = function (size) {
   return {
-    expand (data: Buffer): Buffer {
-      const buffer = crypto.randomBytes(data.length < size - 4 ? size : data.length + 4)
+    expand (data) {
+      const buffer = randomBytes(data.length < size - 4 ? size : data.length + 4)
       buffer.writeUInt32BE(data.length, 0)
       data.copy(buffer, 4, 0)
       return buffer
     },
-    shrink (buffer: Buffer): Buffer {
+    shrink (buffer) {
       const dataLen = buffer.readUInt32BE(0)
       return buffer.slice(4, dataLen + 4)
     }
